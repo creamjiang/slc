55e36d4932d7700337206793e218c4d768728de2
Samson Sprouse <samson.sprouse@gmail.com>
2013-01-13 09:11:15 -0800

    Layout changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -37,7 +37,7 @@ class HomeController < ApplicationController
 
     @title = "Reading"
     @reading_level = "G"
-    @description = "#{student_name_obj['firstName']}'s area of focus:"
+    @description = "Area of focus:"
     @focus_area = "Fluency"
     @focus_area_details = "I am the focus details"
     @focus_area_info = "cc"
